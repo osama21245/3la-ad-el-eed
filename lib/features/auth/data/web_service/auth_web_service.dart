d01cac1c0168ab1a57150a3b84a4ea24db121a34
moemenrady <moemenrady2009@gmail.com>
2025-07-16 19:07:25 +0300

    when create account add it to user collection

--- lib/features/auth/data/web_service/auth_web_service.dart
+++ lib/features/auth/data/web_service/auth_web_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 abstract interface class AuthWebService {
@@ -14,7 +15,9 @@ abstract interface class AuthWebService {
 
 class AuthWebServiceImpl implements AuthWebService {
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-
+  CollectionReference userCollection = FirebaseFirestore.instance.collection(
+    "users",
+  );
   @override
   Future<UserCredential> signUpWithEmailPass({
     required String email,
@@ -22,21 +25,34 @@ class AuthWebServiceImpl implements AuthWebService {
     required String userName,
   }) async {
     try {
+      // create account with emailPassword Firebase Auth
       UserCredential userCredential = await firebaseAuth
           .createUserWithEmailAndPassword(email: email, password: password);
+      // adding user Data to users collection Firebase FireStore
+      await userCollection.add({
+        "user_id": userCredential.user!.uid,
+        "is_seller": 0,
+        "is_send_shop_request": 0,
+        "user_name": userName,
+        "email": email,
+        "password": password,
+      });
       return userCredential;
     } catch (e) {
       throw ">>>> Error When CreateAccount: $e";
     }
   }
+
   @override
   Future<void> signInWithEmailPass({
     required String email,
     required String password,
   }) async {
     try {
-      await firebaseAuth
-          .signInWithEmailAndPassword(email: email, password: password);
+      await firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
     } catch (e) {
       throw ">>>> Error When Login With Email , Pass $e";
     }
