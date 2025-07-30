8ce2f70f53b78c382defd852e087ff54b5353832
karimaIsmail <you@example.comkarimaismail5@gmail.com>
2025-07-30 16:41:05 +0300

    Add driver live tracking using Google Maps

--- lib/features/trip/view/widgets/trip_Screen_body.dart
+++ lib/features/trip/view/widgets/trip_Screen_body.dart
@@ -1,4 +1,4 @@
-import 'package:_3la_ad_el_eed/features/trip/view/controller/cubit/trip_cubit_cubit.dart';
+import 'package:_3la_ad_el_eed/features/trip/view/controller/cubit/trip_cubit.dart';
 import 'package:_3la_ad_el_eed/features/trip/view/controller/cubit/trip_cubit_state.dart';
 import 'package:_3la_ad_el_eed/features/trip/view/widgets/google_map_widget.dart';
 import 'package:_3la_ad_el_eed/features/trip/view/widgets/trip_info_widget.dart';
@@ -9,25 +9,35 @@ class TripScreenBody extends StatelessWidget {
   const TripScreenBody({super.key});
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TripCubitCubit, TripInfoState>(
-      builder: (context, state) {
-        final tripCubit = context.read<TripCubitCubit>();
-        return Stack(
-          children: [
-            GoogleMapWidget(
-              origin: tripCubit.getLatLng(tripCubit.origin),
-              destination: tripCubit.getLatLng(tripCubit.destination),
-            ),
+    return Scaffold(
+      appBar: tripAppBar(),
+      body: BlocBuilder<TripCubit, TripInfoState>(
+        builder: (context, state) {
+          return state.isSuccess
+              ? Stack(
+                children: [
+                  GoogleMapWidget(directionModel: state.directionModel),
 
-            TripInfoWidget(
-              distance: "distance\n ${state.tripInfoModel!.distanceText}",
-              distanceIcon: Icons.route,
-              duration: "duration\n ${state.tripInfoModel!.durationText}",
-              durationIcon: Icons.access_time,
-            ),
-          ],
-        );
-      },
+                  TripInfoWidget(
+                    distance:
+                        "distance\n ${state.directionModel.totalDistance}",
+                    distanceIcon: Icons.route,
+                    duration:
+                        "duration\n ${state.directionModel.totalDuration}",
+                    durationIcon: Icons.access_time,
+                  ),
+                ],
+              )
+              : Container();
+        },
+      ),
     );
   }
+}
+
+AppBar tripAppBar() {
+  return AppBar(
+    title: Text('Trip details', style: TextStyle(color: Colors.blue[700])),
+    centerTitle: true,
+  );
 }
